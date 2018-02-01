af7d28dec060d4da40a1b393482fe3e749724100
stalary <452024236@qq.com>
2018-02-01 23:17:37 +0800

    MOD ：抽离出ListNode

--- src/DeleteDuplication.java
+++ src/DeleteDuplication.java
@@ -33,12 +33,4 @@ public class DeleteDuplication {
         }
     }
 
-    public class ListNode {
-        int val;
-        ListNode next = null;
-
-        ListNode(int val) {
-            this.val = val;
-        }
-    }
 }
---
+++ src/ListNode.java
@@ -0,0 +1,21 @@
+/**
+ * @(#)ListNode.java, 2018-02-01.
+ * <p>
+ * Copyright 2018 Youdao, Inc. All rights reserved.
+ * YOUDAO PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+/**
+ * ListNode
+ *
+ * @author lirongqian
+ * @since 2018/02/01
+ */
+public class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
