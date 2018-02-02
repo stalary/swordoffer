feb2ecd039a5c054e603f4d14cb0f18630bcfa99
stalary <452024236@qq.com>
2018-02-03 00:13:31 +0800

    ADD ：模式匹配

---
+++ src/Match.java
@@ -0,0 +1,53 @@
+/**
+ * @(#)Match.java, 2018-02-02.
+ * <p>
+ * Copyright 2018 Youdao, Inc. All rights reserved.
+ * YOUDAO PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+/**
+ * Match
+ *
+ * 匹配字符串，一共两种模式.和*
+ * 难度较大
+ * @author lirongqian
+ * @since 2018/02/02
+ */
+public class Match {
+
+    public boolean match(char[] str, char[] pattern) {
+        if (str == null || pattern == null) {
+            return false;
+        }
+        return matchCore(str, 0, pattern, 0);
+    }
+
+    public boolean matchCore(char[] str, int strIndex, char[] pattern, int patternIndex) {
+        //有效性检验：str到尾，pattern到尾，匹配成功
+        if (strIndex == str.length && patternIndex == pattern.length) {
+            return true;
+        }
+        //pattern先到尾，匹配失败
+        if (strIndex != str.length && patternIndex == pattern.length) {
+            return false;
+        }
+        //模式第2个是*，且字符串第1个跟模式第1个匹配,分3种匹配模式；如不匹配，模式后移2位
+        if (patternIndex + 1 < pattern.length && pattern[patternIndex + 1] == '*') {
+            if ((strIndex != str.length && pattern[patternIndex] == str[strIndex]) || (pattern[patternIndex] == '.' && strIndex != str.length)) {
+                //模式后移2，视为x*匹配0个字符
+                return matchCore(str, strIndex, pattern, patternIndex + 2)
+                        //视为模式匹配1个字符
+                        || matchCore(str, strIndex + 1, pattern, patternIndex + 2)
+                        //*匹配1个，再匹配str中的下一个
+                        || matchCore(str, strIndex + 1, pattern, patternIndex);
+            } else {
+                return matchCore(str, strIndex, pattern, patternIndex + 2);
+            }
+        }
+        //模式第2个不是*，且字符串第1个跟模式第1个匹配，则都后移1位，否则直接返回false
+        if ((strIndex != str.length && pattern[patternIndex] == str[strIndex]) || (pattern[patternIndex] == '.' && strIndex != str.length)) {
+            return matchCore(str, strIndex + 1, pattern, patternIndex + 1);
+        }
+        return false;
+    }
+}
