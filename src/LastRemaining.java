acb02d6eb7074d5c2c0e295c807eb6ef38110b47
stalary <452024236@qq.com>
2018-02-04 23:00:59 +0800

    ADD ：圆圈中剩下的最后一个人

---
+++ src/LastRemaining.java
@@ -0,0 +1,28 @@
+/**
+ * @(#)LastRemaining.java, 2018-02-04.
+ * <p>
+ * Copyright 2018 Youdao, Inc. All rights reserved.
+ * YOUDAO PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+/**
+ * LastRemaining
+ *
+ * 圆圈中最后剩下的数
+ * @author lirongqian
+ * @since 2018/02/04
+ */
+public class LastRemaining {
+
+    public int LastRemaining_Solution(int n, int m) {
+        if (n < 1 || m < 1) {
+            return -1;
+        }
+        int last = 0;
+        // 通过找规律可以发现每次最后剩下的数字是上次最后剩下的数字加退出的人取模于总人数
+        for (int i = 2; i <= n; i++) {
+            last = (last + m) % i;
+        }
+        return last;
+    }
+}
