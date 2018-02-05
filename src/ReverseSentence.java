d22e66ebb546d41883677178e89561119af1c36d
stalary <452024236@qq.com>
2018-02-06 00:24:57 +0800

    ADD ：翻转字符串

---
+++ src/ReverseSentence.java
@@ -0,0 +1,34 @@
+/**
+ * @(#)ReverseSentence.java, 2018-02-06.
+ * <p>
+ * Copyright 2018 Youdao, Inc. All rights reserved.
+ * YOUDAO PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+/**
+ * ReverseSentence
+ *
+ * 翻转单词
+ * @author lirongqian
+ * @since 2018/02/06
+ */
+public class ReverseSentence {
+
+    /**
+     * 首先截取字符串，然后再进行倒置
+     * @param str
+     * @return
+     */
+    public String ReverseSentence(String str) {
+        if (str == null || str.length() == 0 || "".equals(str.trim())) {
+            return str;
+        }
+        String[] strings = str.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for (int i = strings.length - 1; i > 0; i--) {
+            sb.append(strings[i]).append(" ");
+        }
+        sb.append(strings[0]);
+        return sb.toString();
+    }
+}
