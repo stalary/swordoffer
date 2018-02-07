2ae5363a94d61d7d7349150eec08f60509afa732
stalary <452024236@qq.com>
2018-02-07 11:26:22 +0800

    ADD ：求二叉树的深度

---
+++ src/TreeDepth.java
@@ -0,0 +1,28 @@
+/**
+ * @(#)TreeDepth.java, 2018-02-07.
+ * <p>
+ * Copyright 2018 Youdao, Inc. All rights reserved.
+ * YOUDAO PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+/**
+ * TreeDepth
+ *
+ * 求一颗二叉树的深度
+ * @author lirongqian
+ * @since 2018/02/07
+ */
+public class TreeDepth {
+
+    /**
+     * 直接递归的查找左儿子和右儿子的深度，每次获取左右结点较大一边的值，每深入一层要+1
+     * @param root
+     * @return
+     */
+    public int TreeDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + Math.max(TreeDepth(root.left), TreeDepth(root.right));
+    }
+}
