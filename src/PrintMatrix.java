87389ce49bc10d3f5f4ebfebc643aeaa865a6eb4
stalary <452024236@qq.com>
2018-02-12 17:23:52 +0800

    ADD ：打印矩阵

---
+++ src/PrintMatrix.java
@@ -0,0 +1,56 @@
+/**
+ * @(#)PrintMatrix.java, 2018-02-12.
+ * <p>
+ * Copyright 2018 Youdao, Inc. All rights reserved.
+ * YOUDAO PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+import java.util.ArrayList;
+
+/**
+ * PrintMatrix
+ * <p>
+ * 顺时针打印矩阵
+ *
+ * @author lirongqian
+ * @since 2018/02/12
+ */
+public class PrintMatrix {
+
+    /**
+     * 首先要先求出层数，然后进行遍历
+     * @param matrix
+     * @return
+     */
+    public ArrayList<Integer> printMatrix(int[][] matrix) {
+        ArrayList<Integer> result = new ArrayList<>();
+        if (matrix.length == 0) {
+            return result;
+        }
+        int n = matrix.length, m = matrix[0].length;
+        if (m == 0) {
+            return result;
+        }
+        //层数
+        int layers = (Math.min(n, m) - 1) / 2 + 1;
+        for (int i = 0; i < layers; i++) {
+            for (int k = i; k < m - i; k++) {
+                //左至右
+                result.add(matrix[i][k]);
+            }
+            for (int j = i + 1; j < n - i; j++) {
+                //右上至右下
+                result.add(matrix[j][m - i - 1]);
+            }
+            for (int k = m - i - 2; k >= i && n - i - 1 != i; k--) {
+                //右至左
+                result.add(matrix[n - i - 1][k]);
+            }
+            for (int j = n - i - 2; j > i && m - i - 1 != i; j--) {
+                //左下至左上
+                result.add(matrix[j][i]);
+            }
+        }
+        return result;
+    }
+}
