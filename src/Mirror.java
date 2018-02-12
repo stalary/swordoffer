1e9fad2367827dbe41866d3e7525827c25790469
stalary <452024236@qq.com>
2018-02-12 17:29:47 +0800

    ADD ：二叉树镜像

---
+++ src/Mirror.java
@@ -0,0 +1,31 @@
+/**
+ * @(#)Mirror.java, 2018-02-12.
+ * <p>
+ * Copyright 2018 Youdao, Inc. All rights reserved.
+ * YOUDAO PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+/**
+ * Mirror
+ *
+ * 转化为二叉树的镜像
+ * @author lirongqian
+ * @since 2018/02/12
+ */
+public class Mirror {
+
+    /**
+     * 递归的遍历子树，将左右结点交换
+     * @param root
+     */
+    public void Mirror(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        Mirror(root.left);
+        Mirror(root.right);
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+    }
+}
