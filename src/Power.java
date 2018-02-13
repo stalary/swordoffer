f1b8e8b51712a109075dbf875dc5d2531babbe40
stalary <452024236@qq.com>
2018-02-13 10:08:47 +0800

    ADD ：数值的整数次方

---
+++ src/Power.java
@@ -0,0 +1,38 @@
+/**
+ * @(#)Power.java, 2018-02-13.
+ * <p>
+ * Copyright 2018 Youdao, Inc. All rights reserved.
+ * YOUDAO PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+/**
+ * Power
+ * <p>
+ * 数值的整数次方
+ *
+ * @author lirongqian
+ * @since 2018/02/13
+ */
+public class Power {
+
+    /**
+     * 注意判断负数的时候
+     * @param base
+     * @param exponent
+     * @return
+     */
+    public double Power(double base, int exponent) {
+        if (base == 0) {
+            return 0;
+        }
+        if (exponent == 0) {
+            return 1;
+        }
+        double result = 1.0;
+        int temp = exponent > 0 ? exponent : -exponent;
+        for (int i = 0; i < temp; i++) {
+            result *= base;
+        }
+        return exponent > 0 ? result : 1 / result;
+    }
+}
