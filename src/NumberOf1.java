61cf67105f6b3a8d9f743e875b1c07128a9962cd
stalary <452024236@qq.com>
2018-02-14 11:54:35 +0800

    ADD ：求二进制中1的个数

--- src/NumberOf1.java
+++ src/NumberOf1.java
@@ -5,14 +5,18 @@
  * YOUDAO PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
  */
 
+import java.util.List;
+
 /**
  * NumberOf1
- *
+ * <p>
  * 求二进制中1的个数
+ *
  * @author lirongqian
  * @since 2018/02/13
  */
 public class NumberOf1 {
+
     public int NumberOf1(int n) {
         int count = 0;
         while (n != 0) {
@@ -21,4 +25,5 @@ public class NumberOf1 {
         }
         return count;
     }
+
 }
