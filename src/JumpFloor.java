bb4542685b01c9b0677e55641de7d698093d8035
stalary <452024236@qq.com>
2018-02-14 12:58:38 +0800

    ADD ：跳台阶

---
+++ src/JumpFloor.java
@@ -0,0 +1,29 @@
+/**
+ * @(#)JumpFloor.java, 2018-02-14.
+ * <p>
+ * Copyright 2018 Youdao, Inc. All rights reserved.
+ * YOUDAO PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+/**
+ * JumpFloor
+ *
+ * 跳台阶，可以跳1或者2层，求跳上n层一共有多少种方法
+ * @author lirongqian
+ * @since 2018/02/14
+ */
+public class JumpFloor {
+
+    public int JumpFloor(int target) {
+        if (target <= 2) {
+            return target;
+        }
+        int[] arr = new int[target + 1];
+        arr[0] = 1;
+        arr[1] = 1;
+        for (int i = 2; i <= target; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+        return arr[target];
+    }
+}
