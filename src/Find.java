d7ce19b3d5ff3f0b8bbffb059a2d7fb44399b95e
stalary <452024236@qq.com>
2018-02-15 20:14:12 +0800

    ADD ：二维数组中的查找

---
+++ src/Find.java
@@ -0,0 +1,25 @@
+/**
+ * Find
+ * <p>
+ * 二维数组中的查找
+ *
+ * @author lirongqian
+ * @since 2018/02/15
+ */
+public class Find {
+
+    public boolean Find(int target, int[][] array) {
+        int row = 0;
+        int col = array[0].length - 1;
+        while (col >= 0 && row < array[0].length) {
+            if (target < array[row][col]) {
+                col--;
+            } else if (target > array[row][col]) {
+                row++;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
