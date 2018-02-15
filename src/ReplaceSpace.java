1eaabf7f0e8099f75f621308a332f254e0ab6f9d
stalary <452024236@qq.com>
2018-02-15 20:03:12 +0800

    ADD ：替换空格

---
+++ src/ReplaceSpace.java
@@ -0,0 +1,43 @@
+/**
+ * ReplaceSpace
+ *
+ * 替换空格
+ * @author lirongqian
+ * @since 2018/02/15
+ */
+public class ReplaceSpace {
+
+    /**
+     * 如何直接使用正则表达式替换速度会很慢，所以首先计算出空格的数量，从末尾进行替换
+     * @param str
+     * @return
+     */
+    public String replaceSpace(StringBuffer str) {
+        int blank = 0;
+        int len = str.length();
+        for (int i = 0; i < len; i++) {
+            if (str.charAt(i) == ' ') {
+                blank++;
+            }
+        }
+        int oldLen = len - 1;
+        int newLen = len + 2 * blank;
+        int index = newLen - 1;
+        str.setLength(newLen);
+        // 从末尾开始遍历，当遇到空格时进行替换，非空格时即将原元素放入
+        for (; oldLen >= 0; oldLen--) {
+            if (str.charAt(oldLen) == ' ') {
+                str.setCharAt(index--, '0');
+                str.setCharAt(index--, '2');
+                str.setCharAt(index--, '%');
+                blank--;
+            } else {
+                str.setCharAt(index--, str.charAt(oldLen));
+            }
+            if (blank == 0) {
+                return str.toString();
+            }
+        }
+        return str.toString();
+    }
+}
