4f3cd81cd89f80de1505e0d531cf823f1de1600b
stalary <452024236@qq.com>
2018-03-07 23:51:25 +0800

    MOD ：二叉树中序遍历的下一个结点

--- src/GetNext.java
+++ src/GetNext.java
@@ -1,9 +1,3 @@
-/**
- * @(#)GetNext.java, 2018-02-01.
- * <p>
- * Copyright 2018 Youdao, Inc. All rights reserved.
- * YOUDAO PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- */
 
 /**
  * GetNext
