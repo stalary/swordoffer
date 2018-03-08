d0e48718dbfe5851b645b24a6b66707e8d335323
stalary <452024236@qq.com>
2018-03-08 10:26:41 +0800

    MOD ：合并两个已经排好序的链表，并且要满足单调不减

--- src/Merge.java
+++ src/Merge.java
@@ -1,9 +1,3 @@
-/**
- * @(#)Merge.java, 2018-02-12.
- * <p>
- * Copyright 2018 Youdao, Inc. All rights reserved.
- * YOUDAO PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- */
 
 /**
  * Merge
