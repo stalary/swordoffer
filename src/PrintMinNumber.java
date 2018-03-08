ab1075fe18cfa01ae16ec8ced3180837a30b74db
stalary <452024236@qq.com>
2018-03-08 10:48:32 +0800

    MOD ：将数组中的所有数字拼接，找到一个组合的最小数

--- src/PrintMinNumber.java
+++ src/PrintMinNumber.java
@@ -1,9 +1,3 @@
-/**
- * @(#)PrintMinNumber.java, 2018-02-08.
- * <p>
- * Copyright 2018 Youdao, Inc. All rights reserved.
- * YOUDAO PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- */
 
 import java.util.ArrayList;
 import java.util.List;
