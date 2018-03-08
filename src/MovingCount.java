7dfe771279f86054d7d28c15e5e9828fe34ef3e0
stalary <452024236@qq.com>
2018-03-08 10:33:48 +0800

    MOD ：机器人的移动范围

--- src/MovingCount.java
+++ src/MovingCount.java
@@ -1,9 +1,3 @@
-/**
- * @(#)MovingCount.java, 2018-01-30.
- * <p>
- * Copyright 2018 Youdao, Inc. All rights reserved.
- * YOUDAO PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- */
 
 /**
  * MovingCount
