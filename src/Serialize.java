2684e32f1277ac662bcb06b1d4d2c6cf3bc5246c
stalary <452024236@qq.com>
2018-03-08 10:50:31 +0800

    MOD ：序列化与反序列化二叉树

--- src/Serialize.java
+++ src/Serialize.java
@@ -1,9 +1,3 @@
-/**
- * @(#)Serialize.java, 2018-01-31.
- * <p>
- * Copyright 2018 Youdao, Inc. All rights reserved.
- * YOUDAO PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- */
 
 /**
  * Serialize
