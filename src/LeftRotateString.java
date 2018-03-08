0ad5399c50af6ddcfb79b2d04ea3cca4be237b67
stalary <452024236@qq.com>
2018-03-08 10:20:46 +0800

    MOD ：左移字符串

--- src/LeftRotateString.java
+++ src/LeftRotateString.java
@@ -1,9 +1,3 @@
-/**
- * @(#)LeftRotateString.java, 2018-02-06.
- * <p>
- * Copyright 2018 Youdao, Inc. All rights reserved.
- * YOUDAO PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- */
 
 /**
  * LeftRotateString
