279c90f4e9bd12417768c644803f4a040f921d2c
stalary <452024236@qq.com>
2018-03-08 10:36:32 +0800

    MOD ：按字典序打印出该字符串中字符的所有排列

--- src/Permutation.java
+++ src/Permutation.java
@@ -1,9 +1,3 @@
-/**
- * @(#)Permutation.java, 2018-02-09.
- * <p>
- * Copyright 2018 Youdao, Inc. All rights reserved.
- * YOUDAO PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- */
 
 import java.util.ArrayList;
 import java.util.Collections;
