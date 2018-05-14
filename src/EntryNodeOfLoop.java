ca7f15a3e489ab48fd21c9a05ed46b23b10126bd
stalary <452024236@qq.com>
2018-05-14 22:39:00 +0800

    MOD ：查找一个带环链表的入口

--- src/EntryNodeOfLoop.java
+++ src/EntryNodeOfLoop.java
@@ -9,62 +9,36 @@
 public class EntryNodeOfLoop {
 
     /**
-     * 使用一对快慢指针来判断是否有环，快指针每次移动2，慢指针每次移动1
-     * @param pHead
+     * 从相遇点向后移动，当再次回到这个点的时候，就能统计出环中元素的数量
+     * @param head
      * @return
      */
-    public ListNode meetNode(ListNode pHead) {
-        if (pHead == null || pHead.next == null) {
+    public ListNode entryNodeOfLoop(ListNode head) {
+        if (head == null) {
             return null;
         }
-        // 创建快慢指针
-        ListNode slow = pHead.next;
-        ListNode fast = slow.next;
-        // 当快慢指针都不为空时一直向后遍历
-        while (fast != null && slow != null) {
-            // 当两指针相遇时，返回相遇点
-            if (fast == slow) {
-                return fast;
-            }
+        ListNode slow = head;
+        ListNode fast = head;
+        while (slow != null && fast != null) {
             slow = slow.next;
             fast = fast.next;
-            // 快指针移动一次后要判断是否以及有相遇点，如果没有再继续移动
-            if (fast != slow) {
+            if (fast != null) {
                 fast = fast.next;
             }
+            if (slow == fast) {
+                break;
+            }
         }
-        return null;
-    }
-
-    /**
-     * 从相遇点向后移动，当再次回到这个点的时候，就能统计出环中元素的数量
-     * @param pHead
-     * @return
-     */
-    public ListNode entryNodeOfLoop(ListNode pHead) {
-        // 查找相遇的点
-        ListNode meetNode = meetNode(pHead);
-        if (meetNode == null) {
+        // 存在null时代表无环
+        if (slow == null || fast == null) {
             return null;
         }
-        // 统计从相遇点再次到相遇点的数量，即环内元素的数量
-        int count = 1;
-        ListNode p1 = meetNode;
-        while (p1.next != meetNode) {
-            p1 = p1.next;
-            count++;
+        // 将fast指针重置，当与相遇点再次相遇时
+        fast = head;
+        while (fast != slow) {
+            fast = fast.next;
+            slow = slow.next;
         }
-        // p1先移动环内数量的距离
-        p1 = pHead;
-        for (int i = 0; i < count; i++) {
-            p1 = p1.next;
-        }
-        // 此时p2再从头开始运动，相遇时即为环的入口。
-        ListNode p2 = pHead;
-        while (p2 != p1) {
-            p1 = p1.next;
-            p2 = p2.next;
-        }
-        return p1;
+        return slow;
     }
 }
