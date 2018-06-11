dda8a5a5b70bed860456c1ada910f60b3a398f67
stalary <452024236@qq.com>
2018-06-11 10:32:21 +0800

    MOD： 计算逆序对的数量，前面数字大于后面的数字，即代表一个逆序对

--- src/InversePairs.java
+++ src/InversePairs.java
@@ -39,31 +39,34 @@ public class InversePairs {
         // 设置左侧数组范围为i，右侧数组范围为j
         int i = mid;
         int j = high;
+        // 当前判断的下标
         int locCopy = high;
         while (i >= low && j > mid) {
             // 当前面的元素较大时，统计一下数量
             if (array[i] > array[j]) {
                 // 单块的内部已经排好序，所以如果最小的比后面的大的话，会都大
                 count += j - mid;
+                // 满足逆序对时，左侧元素向前移动
                 copy[locCopy--] = array[i--];
                 //数值过大求余
                 if (count >= 1000000007) {
                     count %= 1000000007;
                 }
             } else {
+                // 当不满足逆序对时，右侧元素向前移动
                 copy[locCopy--] = array[j--];
             }
         }
+        // 左侧元素归位
         for (; i >= low; i--) {
             copy[locCopy--] = array[i];
         }
+        // 右侧元素归位
         for (; j > mid; j--) {
             copy[locCopy--] = array[j];
         }
         // 将元素放回原数组，完成排序
-        for (int s = low; s <= high; s++) {
-            array[s] = copy[s];
-        }
+        System.arraycopy(copy, low, array, low, high + 1 - low);
         return (leftCount + rightCount + count) % 1000000007;
     }
 }
