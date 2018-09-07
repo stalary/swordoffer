a9d45561894eff44e1a594c113ae3fb9597430a2
stalary <452024236@qq.com>
2018-09-07 16:44:14 +0800

    在递增数组中，查找两个数和为s，且乘积最小的数

--- src/FindNumbersWithSum.java
+++ src/FindNumbersWithSum.java
@@ -4,7 +4,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
- * FindNumbersWithSum
+ * findNumbersWithSum
  * <p>
  * 在递增数组中，查找两个数和为s，且乘积最小的数
  *
@@ -17,10 +17,10 @@ public class FindNumbersWithSum {
         int[] array = new int[] {
                 1, 3, 5, 6, 8, 9
         };
-        System.out.println(new FindNumbersWithSum().FindNumbersWithSum(array, 11));
+        System.out.println(new FindNumbersWithSum().findNumbersWithSum1(array, 11));
     }
 
-    public ArrayList<Integer> FindNumbersWithSum(int[] array, int sum) {
+    public ArrayList<Integer> findNumbersWithSum(int[] array, int sum) {
         Set<Integer> set = new HashSet<>();
         ArrayList<Integer> result = new ArrayList<>();
         int min = Integer.MAX_VALUE;
@@ -40,5 +40,40 @@ public class FindNumbersWithSum {
             result.add(temp);
         }
         return result;
+    }
+
+    /**
+     * 当和小于目标值时，起点移动，大于时终点移动，否则比较乘积是否最大
+     * @param array
+     * @param sum
+     * @return
+     */
+    public ArrayList<Integer> findNumbersWithSum1(int[] array, int sum) {
+        int start = 0;
+        int end = array.length - 1;
+        int min = Integer.MAX_VALUE;
+        ArrayList<Integer> list = new ArrayList<>();
+        int s = 0;
+        int e = 0;
+        while (start < end) {
+            int result = array[start] + array[end];
+            if (result == sum) {
+                int temp = array[start] * array[end];
+                if (temp < min) {
+                    s = start;
+                    e = end;
+                    min = temp;
+                }
+                start++;
+                end--;
+            } else if (result < sum) {
+                start++;
+            } else {
+                end--;
+            }
+        }
+        list.add(array[s]);
+        list.add(array[e]);
+        return list;
     }
 }
