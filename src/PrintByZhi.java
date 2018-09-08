ecad3d7a21a4563fbc04e5aff90e1038144dd53f
stalary <452024236@qq.com>
2018-09-08 22:50:20 +0800

    按之字型打印二叉树

--- src/PrintByZhi.java
+++ src/PrintByZhi.java
@@ -1,7 +1,6 @@
-
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Stack;
 
 /**
@@ -22,15 +21,75 @@ public class PrintByZhi {
         root.left.left = new TreeNode(5);
         root.left.right = new TreeNode(7);
         root.right.left = new TreeNode(9);
-        root.right.right = new TreeNode(11);
-        System.out.println("stack: " + new PrintByZhi().Print(root));
+        TreeNode root1 = new TreeNode(8);
+        root1.left = new TreeNode(6);
+        root1.right = new TreeNode(10);
+        root1.left.left = new TreeNode(5);
+        root1.left.right = new TreeNode(7);
+        root1.right.left = new TreeNode(9);
+        root1.right.right = new TreeNode(11);
+        root.right.right = root1;
+        long start1 = System.currentTimeMillis();
+        System.out.println("queue: " + new PrintByZhi().print1(root));
+        System.out.println("queue time." + (System.currentTimeMillis() - start1));
+        long start2 = System.currentTimeMillis();
+        System.out.println("stack: " + new PrintByZhi().print(root));
+        System.out.println("stack time." + (System.currentTimeMillis() - start2));
     }
+
+
+    public ArrayList<ArrayList<Integer>> print1(TreeNode root) {
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        // 使用null作为层分隔符
+        queue.addLast(null);
+        queue.addLast(root);
+        boolean leftToRight = true;
+        while (queue.size() != 1) {
+            ArrayList<Integer> list = new ArrayList<>();
+            TreeNode node = queue.removeFirst();
+            // 到达层分隔符
+            if (node == null) {
+                Iterator<TreeNode> it;
+                // 从左往右打印
+                if (leftToRight) {
+                    it = queue.iterator();
+                } else {
+                    // 从右往左打印
+                    it = queue.descendingIterator();
+                }
+                // 反转打印顺序
+                leftToRight = !leftToRight;
+                while (it.hasNext()) {
+                    TreeNode temp = it.next();
+                    list.add(temp.val);
+                }
+                result.add(list);
+                // 添加层分隔符
+                queue.addLast(null);
+                // 直接跳到下一次循环
+                continue;
+            }
+            if (node.left != null) {
+                queue.addLast(node.left);
+            }
+            if (node.right != null) {
+                queue.addLast(node.right);
+            }
+        }
+        return result;
+    }
+
     /**
      * 按之字型打印二叉树，使用两个栈，一个存储奇数的层，一个存储偶数的层，队列无法实现，队列无法实现内部顺序倒置
+     *
      * @param root
      * @return
      */
-    public ArrayList<ArrayList<Integer>> Print(TreeNode root) {
+    public ArrayList<ArrayList<Integer>> print(TreeNode root) {
         int layer = 1;
         // 奇数
         Stack<TreeNode> odd = new Stack<>();
